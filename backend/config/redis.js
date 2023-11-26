4f618aecb48ba7e62624e82abfbf95cf549ec3d6
ErkanE <46621897+erkanErbayram@users.noreply.github.com>
2023-11-26 22:28:57 +0300

    Update redis.js

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,5 +1,10 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient();
+(async () => {
+  await redisClient.connect();
+})();
 
+redisClient.on("connect", () => console.log("Redis Client Connected"));
+redisClient.on("error", (err) => console.log("Redis Client Connection Error", err));
 module.exports = redisClient;
